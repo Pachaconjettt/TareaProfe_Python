7d701991939e00b845a7a5bedf9e1d6cb831cec0
Pachaconjettt <efrain.retana.segura@est.una.ac.cr>
2024-11-28 22:34:30 -0600

    Faltan 2 Ejercicios más a la ultima saemana

--- TareaProfe_Python/Sexta Semana/Sexta semana.py
+++ TareaProfe_Python/Sexta Semana/Sexta semana.py
@@ -20,7 +20,7 @@ while(contNumeros <= cantNumeros):
 
 print("El promedio es:", sumaTotal / cantNumeros)
 
-input("Teclee una tecla para continuar...")
+input("Toque una tecla para continuar......")
 os.system("cls")
 
 print("\t\t Segundo Ejercicio \t\t\n")
@@ -42,4 +42,77 @@ while(bandera):
         bandera = False
 
 print("El promedio es :", sumTotal / cantNumerosv2)
-           
+
+input("Toque una tecla para continuar......")
+os.system("cls")
+
+print("\t\t Tercer Ejercicio \t\t\n")
+
+N = 0
+N = int(input("Digite el valor de N: "))
+
+while(N >= 1): 
+    print("El valor es: ", N)
+    N-= 1
+    count = 0
+while(count <= N):
+    print("El valor es :", count)
+    count += 1
+
+input("Toque una tecla para continuar.......")
+os.system("cls")
+
+print("\t\t Cuarto Ejercicio \t\t\n")
+
+factorial = 1
+number = 0
+cont = 1
+
+number = int(input("Digite el valor de N :"))
+while(cont <= number ):
+    factorial *= cont
+    cont+= 1
+
+print("El valor del factorial de :", number , "es :", factorial)
+
+input("Toque una tecla para continuar con los ejercicios.....")
+os.system("cls")
+
+print("\t\t Quinto Ejercicio \t\t\n")
+
+
+cont = 1
+N = int(input("Digite el valor de N :"))
+while(cont <= (N/2)):
+    if((N % cont) == 0):
+        print("Divisor :", cont)
+    cont += 1 
+print("Divisor :", N)
+
+input("Toque una tecla para continuar......")
+os.system("cls")
+
+print("\t\t Sexto Ejercicio \t\t\n")
+
+ac = 0
+esPar = True
+cont = 1
+
+while(esPar):
+    if(cont % 2 != 0):
+       esPar = False
+    else:
+        cont += 1
+if(esPar == False):
+    print("Hubo un valor impar")
+
+while(cont <= 10):
+    print(cont)
+    ac+=cont 
+  
+    cont+=1 
+
+print("El valor de ac es de :", ac)
+
+input("\t\t Septimo Ejercicio \t\t\n")
+
